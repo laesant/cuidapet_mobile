f46b9f82d19babc9970bd220011ef55a8034b4dd
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-04-29 18:10:06 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/blacknoise/cuidapet_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.blacknoise.cuidapet_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:asuka/asuka.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setObservers([Asuka.asukaHeroController]);
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      useInheritedMediaQuery: true,
+      builder: (_, __) => MaterialApp.router(
+        title: 'Cuidapet',
+        debugShowCheckedModeBanner: false,
+        builder: Asuka.builder,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
+      ),
+    );
+  }
+}
