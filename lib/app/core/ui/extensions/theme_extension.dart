b5682e52f73fcd2e3be3d9356567a5d03c3099c3
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-05-02 11:51:17 -0300

    Update theme_extension.dart

--- lib/app/core/ui/extensions/theme_extension.dart
+++ lib/app/core/ui/extensions/theme_extension.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 extension ThemeExtension on BuildContext {
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
   Color get primaryColor => Theme.of(this).colorScheme.primary;
   Color get primaryColorDark => Theme.of(this).primaryColorDark;
   Color get primaryColorLight => Theme.of(this).primaryColorLight;
