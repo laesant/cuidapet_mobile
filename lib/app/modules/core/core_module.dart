5cf721f48362718ce71af4eb0e56230542e02929
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-05-12 13:36:44 -0300

    Update core_module.dart

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,3 +1,5 @@
+import 'package:cuidapet_mobile/app/core/rest_cliente/dio/dio_rest_client.dart';
+import 'package:cuidapet_mobile/app/core/rest_cliente/rest_client.dart';
 import 'package:cuidapet_mobile/app/modules/core/auth/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -5,6 +7,7 @@ class CoreModule extends Module {
   @override
   void exportedBinds(Injector i) {
     i.addLazySingleton(AuthStore.new);
+    i.addLazySingleton<RestClient>(DioRestClient.new);
     super.exportedBinds(i);
   }
 }
