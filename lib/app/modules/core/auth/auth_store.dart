f8a2803c95e1918fe9210702ea7c1f68e5572a50
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-06-21 15:47:10 -0300

    Acertos no logout do projeto

--- lib/app/modules/core/auth/auth_store.dart
+++ lib/app/modules/core/auth/auth_store.dart
@@ -1,6 +1,7 @@
 import 'package:cuidapet_mobile/app/core/helpers/constants.dart';
 import 'package:cuidapet_mobile/app/core/local_storage/local_storage.dart';
 import 'package:cuidapet_mobile/app/models/user_model.dart';
+import 'package:cuidapet_mobile/app/services/address/address_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobx/mobx.dart';
 part 'auth_store.g.dart';
@@ -9,12 +10,19 @@ class AuthStore = AuthStoreBase with _$AuthStore;
 
 abstract class AuthStoreBase with Store {
   final LocalStorage _localStorage;
+  final LocalSecureStorage _secureStorage;
+  final AddressService _addressService;
 
   @readonly
   UserModel? _userLogged;
 
-  AuthStoreBase({required LocalStorage localStorage})
-      : _localStorage = localStorage;
+  AuthStoreBase(
+      {required LocalStorage localStorage,
+      required LocalSecureStorage secureStorage,
+      required AddressService addressService})
+      : _localStorage = localStorage,
+        _secureStorage = secureStorage,
+        _addressService = addressService;
 
   @action
   Future<void> loadUserLogged() async {
@@ -36,6 +44,8 @@ abstract class AuthStoreBase with Store {
   @action
   Future<void> logout() async {
     await _localStorage.clear();
+    await _secureStorage.clear();
+    await _addressService.deleteAll();
     _userLogged = UserModel.empty();
   }
 }
