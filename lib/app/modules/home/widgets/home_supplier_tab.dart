c4eb46ce51884fca87ca69e288041814be6f515a
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-06-28 10:58:43 -0300

    Implementando layout da lista de Pet Shops

--- lib/app/modules/home/widgets/home_supplier_tab.dart
+++ lib/app/modules/home/widgets/home_supplier_tab.dart
@@ -9,16 +9,18 @@ class _HomeSupplierTab extends StatelessWidget {
     return Column(
       children: [
         _HomeTabHeader(controller),
-        Observer(
-          builder: (_) {
-            return AnimatedSwitcher(
-              duration: const Duration(milliseconds: 400),
-              child:
-                  controller.supplierPageTypeSelected == SupplierPageType.list
-                      ? const _HomeSupplierList()
-                      : const _HomeSupplierGrid(),
-            );
-          },
+        Expanded(
+          child: Observer(
+            builder: (_) {
+              return AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                child:
+                    controller.supplierPageTypeSelected == SupplierPageType.list
+                        ? const _HomeSupplierList()
+                        : const _HomeSupplierGrid(),
+              );
+            },
+          ),
         )
       ],
     );
@@ -76,7 +78,106 @@ class _HomeSupplierList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Supplier List ');
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 10,
+            (context, index) {
+              return _HomeSupplierListItemWidget();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HomeSupplierListItemWidget extends StatelessWidget {
+  const _HomeSupplierListItemWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Stack(
+        children: [
+          Container(
+            height: 80.h,
+            width: 1.sw,
+            margin: const EdgeInsets.only(left: 30),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 50),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Clinica Central ABC',
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, size: 16),
+                            Text('1.34km de distância'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: CircleAvatar(
+                      backgroundColor: context.primaryColorLight,
+                      maxRadius: 15,
+                      child: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 15,
+                      )),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 70,
+            height: 70,
+            margin: const EdgeInsets.only(top: 5),
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(
+                width: 1,
+                color: Colors.transparent,
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 5,
+                  color: Colors.grey.shade100,
+                ),
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(100),
+                image: const DecorationImage(
+                  fit: BoxFit.contain,
+                  image: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQskONOFOspkbTbOrVbW2yONt1S5rPIRzcdNA&shttps://img.nsctotal.com.br/wp-content/uploads/2023/12/Golden-retriever.jpg'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
