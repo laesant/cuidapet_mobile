e16f3b77d77787a71cffb43d6e456b41c5b5bd6d
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-07-01 11:09:30 -0300

    Adicionando titulo quando appBar Collapsed

--- lib/app/modules/supplier/supplier_page.dart
+++ lib/app/modules/supplier/supplier_page.dart
@@ -1,9 +1,37 @@
+import 'package:cuidapet_mobile/app/core/ui/extensions/theme_extension.dart';
 import 'package:cuidapet_mobile/app/modules/supplier/widgets/supplier_detail.dart';
 import 'package:cuidapet_mobile/app/modules/supplier/widgets/supplier_service_widget.dart';
 import 'package:flutter/material.dart';
 
-class SupplierPage extends StatelessWidget {
+class SupplierPage extends StatefulWidget {
   const SupplierPage({super.key});
+
+  @override
+  State<SupplierPage> createState() => _SupplierPageState();
+}
+
+class _SupplierPageState extends State<SupplierPage> {
+  late final ScrollController _scrollController;
+  bool sliverCollapsed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.offset > 180 &&
+          !_scrollController.position.outOfRange) {
+        setState(() {
+          sliverCollapsed = true;
+        });
+      } else if (_scrollController.offset <= 180 &&
+          !_scrollController.position.outOfRange) {
+        setState(() {
+          sliverCollapsed = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +43,21 @@ class SupplierPage extends StatelessWidget {
         label: const Text('Fazer agendamento'),
       ),
       body: CustomScrollView(
+        controller: _scrollController,
         slivers: [
           SliverAppBar(
             pinned: true,
             expandedHeight: 200,
-            leading: const BackButton(color: Colors.white),
+            leading: Visibility(
+                visible: sliverCollapsed,
+                replacement: const CircleAvatar(
+                  child: BackButton(),
+                ),
+                child: const BackButton()),
+            title: Visibility(
+              visible: sliverCollapsed,
+              child: const Text('Clinica Central ABC'),
+            ),
             flexibleSpace: FlexibleSpaceBar(
               stretchModes: const [
                 StretchMode.fadeTitle,
